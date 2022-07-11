0fb4f494f63ccde0f9211e774fd610cc45a4cbe0
mattzteh <mattzteh@gmail.com>
2022-07-11 16:37:02 -0700

    phase 3 done

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    if !self.include?(key)
+      self[key] << key
+      @count += 1
+    end
+    if @count > num_buckets
+      resize!
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    i = num.hash % num_buckets
+    @store[i]
   end
 
   def num_buckets
@@ -26,5 +39,9 @@ class HashSet
   end
 
   def resize!
+     new_buckets = num_buckets * 2
+     new_arr = Array.new(new_buckets) { Array.new }
+     @store.flatten.each {|ele| new_arr << ele}
+     @store = new_arr
   end
 end

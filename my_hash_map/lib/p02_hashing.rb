7c2ee78facce10845fbf5db519121d71fcac54c4
mattzteh <mattzteh@gmail.com>
2022-07-11 15:49:40 -0700

    phase 2 done

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    return 1 if self.empty?
+    new_arr = []
+    self.each_with_index do |ele, i|
+      new_arr << ele.hash + i.hash
+    end
+    new_arr.sum % new_arr[self.length % self.last]
   end
 end
 
 class String
   def hash
+    new_arr = []
+    self.each_char.with_index do |ele, i|
+      # new_arr << alphabet.index(ele)
+      new_arr << ele.ord
+    end
+    new_arr.sum % new_arr.last
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each do |k, v|
+      sum += k.hash + v.hash
+    end
+    sum
   end
 end
